bc19b4b8b0d18b4b3681ecbe84d3c3ef4e889433
leviovercast <levi.overcast@gmail.com>
2017-02-01 15:30:06 -0800

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,1 +1,31 @@
+import java.util.*;
+/**
+*
+* 
+*
+*
+*/
+public class Car {
+   private int pos;
+   private int gas;
+   private int dist;
 
+   public Car(int pos, int gas, int dist) {
+      this.pos = pos;
+      this.gas = gas;
+      this.dist = dist;
+   }
+
+   public int getPos() {return this.pos;}
+   public int getGas() {return this.gas;}
+   public int getDist() {return this.dist;}
+
+   public String toString(){
+      String toStr = "";
+      toStr += String.format("Car position: %d; ",this.pos);
+      toStr += String.format("gas: %d; ", this.gas);
+      toStr += String.format("distance: %d.", this.dist);
+      return toStr;
+   }
+
+}
